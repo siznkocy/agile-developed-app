f20256d7db0cfc71b7cb637cc79c2e579cd6b356
siznkocy <siznkocie@gmail.com>
2023-06-04 10:02:40 +0200

    testing #@

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="bg-black h-screen grid place-content-center">
       <h1 className="">NOstra</h1>
+      <h2>agile method??!</h2>
     </main>
   );
 }
