574654363f684fbd05304b048f312fc49a395cd2
siznkocy <siznkocie@gmail.com>
2023-06-04 09:19:46 +0200

    testing #1

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="bg-red-600 h-screen">
-      <h1 className="bg-gradient-to-t from-black h-[50%] w-[20%]">
-        Hi, I'm Sizwe N.
-      </h1>
+    <main className="bg-black h-screen">
+      <h1 className="flex">Hi, I'm Sizwe N.</h1>
     </main>
   );
 }
